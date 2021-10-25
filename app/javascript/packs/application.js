16e1c9119d9905279d0037e53f4e5d8b3627156d
Samsondeen Dare <dsamsondeen@gmail.com>
2021-10-25 13:14:57 +0200

    add stock model and api key

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+
+var jQuery = require("jquery");
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+import "bootstrap";
